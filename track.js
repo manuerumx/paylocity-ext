76c17e12eb438bf3e7a79cc3e872980b9ba4f754
Manuel Gonzalez <mgrivera@gmail.com>
2020-01-02 13:37:50 -0600

    Small fixes and jsdoc

--- track.js
+++ track.js
@@ -1,22 +1,33 @@
 document.addEventListener('DOMContentLoaded', function () {
+    let checkPageButton = document.getElementById('refresh');
+
     checkStorage();
-    var checkPageButton = document.getElementById('refresh');
+    updateTime();
+
     checkPageButton.addEventListener('click', function () {
         updateTime();
     });
-    updateTime();
 }, false);
 
+/**
+ * Check in the storage if a maximum number of hours are already defined.
+ * If not, set the value to 45.
+ */
 function checkStorage() {
     chrome.storage.sync.get(['max_hours'], function (result) {
-        if (result === undefined) {
-            chrome.storage.sync.set({'max_hours': 45});
+        let max_hours = 45;
+        if (result.max_hours === undefined) {
+            chrome.storage.sync.set({'max_hours': max_hours});
         } else {
-            document.getElementById('default_hours').value = result.max_hours;
+            max_hours = result.max_hours;
         }
+        document.getElementById('default_hours').value = max_hours;
     });
 }
 
+/**
+ * Update the html with the current hours worked.
+ */
 function updateTime() {
     chrome.tabs.getSelected(null, function (tab) {
         if (validateSite(tab.url)) {
@@ -25,10 +36,21 @@ function updateTime() {
     });
 }
 
+/**
+ * Validate if the current url is valid.
+ *
+ * @param url
+ * @returns {boolean}
+ */
 function validateSite(url) {
     return url === 'https://webtime2.paylocity.com/WebTime/Employee/Timesheet';
 }
 
+/**
+ * Calculate the amount of hours worked, pending and time to leave the office if you want to reach the goal in one session.
+ *
+ * @param accumulated
+ */
 function calculateHours(accumulated) {
     let max_hours = document.getElementById('default_hours').value;
     chrome.storage.sync.set({'max_hours': max_hours});
@@ -40,12 +62,20 @@ function calculateHours(accumulated) {
     let total_minutes = (pending_minutes + parseInt(pending_hours * 60));
     today.setMinutes(today.getMinutes() + total_minutes);
 
+    let hours_accumulated = parseInt(accumulated);
+    let minutes_accumulated = parseInt((accumulated % 1) * 60);
+
     document.getElementById('default_hours').value = max_hours;
-    document.getElementById('total_hrs_worked').innerHTML = parseFloat(accumulated).toFixed(2);
+    document.getElementById('total_hrs_worked').innerHTML = hours_accumulated.toString().padStart(2, '0') + ':' + minutes_accumulated.toString().padStart(2, '0');
     document.getElementById('today_pending').innerHTML = pending_hours.toString().padStart(2, '0') + ':' + pending_minutes.toString().padStart(2, '0');
     document.getElementById('must_leave_office').innerHTML = today.toLocaleTimeString();
 }
 
+/**
+ * Read the DOM and get the totals for the current period. Accumulate with the today hours worked.
+ *
+ * @returns {number}
+ */
 function getAccumulated() {
     let date = new Date();
     let worked = parseFloat(document.querySelectorAll('#GroupTotals td:first-child')[0].innerText.replace(/[^\d.-]/g, ''));
